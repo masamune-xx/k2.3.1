b7c0ed42709e00df9d2ad3c1a41dbdf254aecf01
masamune <emopanda1337@gmail.com>
2023-09-13 20:39:16 +0700

    +password

--- src/main/java/web/config/DBConfig.java
+++ src/main/java/web/config/DBConfig.java
@@ -1,8 +1,6 @@
 package web.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
--- src/main/java/web/model/User.java
+++ src/main/java/web/model/User.java
@@ -1,7 +1,5 @@
 package web.model;
 
-import jakarta.validation.constraints.NotEmpty;
-
 import javax.persistence.*;
 import java.util.Objects;
 
@@ -14,24 +12,33 @@ public class User {
     private int id;
 
     @Column(name = "first_name")
-    @NotEmpty(message = "Required field")
     private String firstName;
 
-    @Column(name = "second_name")
-    @NotEmpty(message = "Required field")
-    private String secondName;
+    @Column(name = "last_name")
+    private String lastName;
 
     @Column(name = "age")
-    @NotEmpty(message = "Required field")
     private int age;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
 
     public User() {
     }
 
-    public User(String firstName, String secondName, int age) {
+    public User(String firstName,
+                String lastName,
+                int age,
+                String email,
+                String password) {
         this.firstName = firstName;
-        this.secondName = secondName;
+        this.lastName = lastName;
         this.age = age;
+        this.email = email;
+        this.password = password;
     }
 
     public int getId() {
@@ -50,12 +57,12 @@ public class User {
         this.firstName = firstName;
     }
 
-    public String getSecondName() {
-        return secondName;
+    public String getLastName() {
+        return lastName;
     }
 
-    public void setSecondName(String secondName) {
-        this.secondName = secondName;
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 
     public int getAge() {
@@ -66,9 +73,31 @@ public class User {
         this.age = age;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     @Override
     public String toString() {
-        return "User [" + id + ":" + firstName + ":" + secondName + ":" + age + "]";
+        return "User ["
+                + id + ":"
+                + firstName + ":"
+                + lastName + ":"
+                + age + ":"
+                + email + ":"
+                + password + "]";
     }
 
     @Override
@@ -76,11 +105,15 @@ public class User {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         User user = (User) o;
-        return age == user.age && Objects.equals(firstName, user.firstName) && Objects.equals(secondName, user.secondName);
+        return age == user.age
+                && Objects.equals(firstName, user.firstName)
+                && Objects.equals(lastName, user.lastName)
+                && Objects.equals(email, user.email)
+                && Objects.equals(password, user.password);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(firstName, secondName, age);
+        return Objects.hash(firstName, lastName, age, email, password);
     }
 }
